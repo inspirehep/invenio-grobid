987277198990b6d0736ec9e4467a01278a6e4fd3
Jacopo Notarstefano <jacopo.notarstefano@cern.ch>
2015-09-23 16:11:15 +0200

    views: initial interface improvement
    Signed-off-by: Jacopo Notarstefano <jacopo.notarstefano@cern.ch>

---
+++ invenio_grobid/static/js/grobid/grobid.js
@@ -0,0 +1,85 @@
+/*
+ * This file is part of Invenio.
+ * Copyright (C) 2015 CERN.
+ *
+ * Invenio is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * Invenio is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with Invenio. If not, see <http://www.gnu.org/licenses/>.
+ *
+ * In applying this licence, CERN does not waive the privileges and immunities
+ * granted to it by virtue of its status as an Intergovernmental Organization
+ * or submit itself to any jurisdiction.
+ */
+
+define([
+  'jquery',
+  'hgn!js/grobid/templates/error',
+  'hgn!js/grobid/templates/result',
+  'hgn!js/grobid/templates/waiting',
+], function ($, errorTemplate, resultTemplate, waitingTemplate) {
+  return {
+    'GROBID': function (opts) {
+      var options = {};
+
+      this.initGROBID = function (opts) {
+        options['error_title'] = opts['error_title'] ||
+          'GROBID has encountered an error.';
+        options['error_explanation'] = opts['error_explanation'] ||
+          'Please try again with the same PDF<br/>or inform the Invenio Developers.';
+        options['url'] = opts['url'] || '/grobid/upload';
+
+        this.handleFile();
+        this.handleSubmit();
+      };
+
+      this.handleFile = function () {
+        $('.grobid-file :file').on('change', function (e) {
+          // See: http://davidwalsh.name/fakepath
+          var fileName = $(this).val().replace('C:\\fakepath\\', '');
+
+          $('.grobid-text').val(fileName);
+        });
+      };
+
+      this.handleSubmit = function () {
+        $('.grobid-form').on('submit', function (e) {
+          e.preventDefault();
+
+          var formData = new FormData($('.grobid-form')[0]);
+
+          $.ajax({
+            type: 'POST',
+            url: options['url'],
+            data: formData,
+            contentType: false,
+            processData: false,
+            dataType: 'xml'
+          }).success(function (data) {
+            // See: http://stackoverflow.com/a/1675058/374865
+            var xml = data.xml ? data.xml : (new XMLSerializer()).serializeToString(data);
+
+            $('.container-results').html(resultTemplate({xml: xml}));
+          }).fail(function (data) {
+            $('.container-results').html(errorTemplate({
+              error_title: options['error_title'],
+              error_explanation: options['error_explanation'],
+            }));
+          });
+
+          $('.container-results').html(waitingTemplate);
+        });
+      };
+
+      this.initGROBID(opts);
+    },
+  };
+});
